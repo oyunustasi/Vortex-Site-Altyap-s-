bf9c653c55fe8a77c6cf92b4e6e8cbafa525d51e
Glitch (vortex-botlistcodevortex-botlistcodeshare) <none>
2021-06-24 17:17:21 +0000

    🐗🚈 Checkpoint ./config.js:31861870/3926

--- config.js
+++ config.js
@@ -1,55 +1,75 @@
 module.exports = {
-    bot: {
-        token: "",
-        prefix: "-",
-        owners: ["552154968426283024"],
-        mongourl: "mongodb+srv://oyun_ustasi:yunusctn34@botlistcodeshare.xgdqp.mongodb.net/BotlistCodeShare?retryWrites=true&w=majority"
-    },
+  bot: {
+    token: "",
+    prefix: "-",
+    owners: ["552154968426283024"],
+    mongourl:
+      "mongodb+srv://oyun_ustasi:yunus@botlistcodeshare.xgdqp.mongodb.net/BotlistCodeShare?retryWrites=true&w=majority"
+  },
 
-    website: {
-        callback: "https://vortex-botlistcodeshare.glitch.me/callback",
-        secret: "rPwOeQ6UymoJkqRJihU58w3zprFa4t7v",
-        clientID: "841265518458699797",
-        tags: [ "Moderasyon", "Eğlence", "Minecraft","Ekonomi","Güvenlik","Level","Anime","Davet","Müzik","Karşılama", "Web Gösterge Paneli", "Reddit", "Youtube", "Twitch", "Kripto", "Logo", "Oyun", "Teknoloji", "Fortnite", "Türkçe" ]    
-    },
+  website: {
+    callback: "https://vortex-botlistcodeshare.glitch.me/callback",
+    secret: "rPwOeQ6UymoJkqRJihU58w3zprFa4t7v",
+    clientID: "841265518458699797",
+    tags: [
+      "Moderasyon",
+      "Eğlence",
+      "Minecraft",
+      "Ekonomi",
+      "Güvenlik",
+      "Level",
+      "Anime",
+      "Davet",
+      "Müzik",
+      "Karşılama",
+      "Web Gösterge Paneli",
+      "Reddit",
+      "Youtube",
+      "Twitch",
+      "Kripto",
+      "Logo",
+      "Oyun",
+      "Teknoloji",
+      "Fortnite",
+      "Türkçe"
+    ]
+  },
 
-    server: {
-        id: "818192263079002163",
-        roles: {
-            yonetici: "841423581387685890",
-            moderator: "841710349303808020",
-            profile: {
-                booster: "841579647896780820",
-                sponsor: "841579892843741225",
-                supporter: "841580172871598092",
-                partnerRole: "856799134672224266"
-            },
-            codeshare: {
-                javascript: "841577207087890453",
-                html: "841577763201613854",
-                altyapilar: "841577775688974336",
-                bdfd: "841579334661832724",
-                besdavet: "841578490197573672",
-                ondavet: "841578505586212894",
-                onbesdavet: "841578510203486228",
-                yirmidavet: "841578513772969985"
-            },
-            botlist: {
-                developer: "841580627056394281",
-                certified_developer: "841580636472999937",
-                bot: "841581055991087115",
-                certified_bot: "841581110390947840"
-            }
-        },
-        channels: {
-            codelog: "841598454228320256",
-            login: "841697860902846514",
-            webstatus: "841697375457247272",
-            uptimelog: "841697277278289940",
-            botlog: "841696807494877256",
-            votes: "841697141289910322"
-        }
+  server: {
+    id: "818192263079002163",
+    roles: {
+      yonetici: "841423581387685890",
+      moderator: "841710349303808020",
+      profile: {
+        booster: "841579647896780820",
+        sponsor: "841579892843741225",
+        supporter: "841580172871598092",
+        partnerRole: "856799134672224266"
+      },
+      codeshare: {
+        javascript: "841577207087890453",
+        html: "841577763201613854",
+        altyapilar: "841577775688974336",
+        bdfd: "841579334661832724",
+        besdavet: "841578490197573672",
+        ondavet: "841578505586212894",
+        onbesdavet: "841578510203486228",
+        yirmidavet: "841578513772969985"
+      },
+      botlist: {
+        developer: "841580627056394281",
+        certified_developer: "841580636472999937",
+        bot: "841581055991087115",
+        certified_bot: "841581110390947840"
+      }
+    },
+    channels: {
+      codelog: "841598454228320256",
+      login: "841697860902846514",
+      webstatus: "841697375457247272",
+      uptimelog: "841697277278289940",
+      botlog: "841696807494877256",
+      votes: "841697141289910322"
     }
-
-
-}
+  }
+};
